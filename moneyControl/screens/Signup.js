c400582deba9781937eb14cbd4667df1532df6bf
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-11-01 11:52:36 +0530

    added validation in signup pahge

--- moneyControl/screens/Signup.js
+++ moneyControl/screens/Signup.js
@@ -3,113 +3,167 @@ import {StyleSheet, Text, View} from 'react-native';
 import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {Formik} from 'formik';
 
-class Signup extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.heading}>Sign up</Text>
-          <Text style={styles.sub_heading}>Welcome to all</Text>
-        </View>
-        <View style={styles.body}>
-          <View style={[styles.text_container, {marginBottom: 15}]}>
-            <View style={styles.text_box}>
-              <Fontisto
-                name="person"
-                size={30}
-                color="#93278f"
-                style={{alignSelf: 'center'}}
-              />
-              <TextInput
-                style={{marginLeft: 10, flexGrow: 1}}
-                placeholder="Full name"
-              />
-            </View>
+import schema from '../schema/signupSchema';
+
+const Signup = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        mobile_number: '',
+        email: '',
+        password: '',
+        confirm_password: '',
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}>
+      {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.heading}>Sign up</Text>
+            <Text style={styles.sub_heading}>Welcome to all</Text>
           </View>
-          <View style={[styles.text_container, {marginBottom: 15}]}>
-            <View style={styles.text_box}>
-              <Fontisto
-                name="mobile-alt"
-                size={30}
-                color="#93278f"
-                style={{alignSelf: 'center'}}
-              />
-              <TextInput
-                keyboardType="number-pad"
-                style={{marginLeft: 10, flexGrow: 1}}
-                placeholder="Mobile number"
-              />
+          <View style={styles.body}>
+            <View style={[styles.text_container, {marginBottom: 15}]}>
+              <View style={styles.text_box}>
+                <Fontisto
+                  name="person"
+                  size={30}
+                  color="#93278f"
+                  style={{alignSelf: 'center'}}
+                />
+                <TextInput
+                  style={{marginLeft: 10, flexGrow: 1}}
+                  placeholder="Full name"
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                />
+              </View>
+              {errors.name ? (
+                <Text style={styles.error_message}>* {errors.name}</Text>
+              ) : null}
             </View>
-          </View>
-          <View style={[styles.text_container, {marginBottom: 15}]}>
-            <View style={styles.text_box}>
-              <Fontisto
-                name="email"
-                size={20}
-                color="#93278f"
-                style={{alignSelf: 'center'}}
-              />
-              <TextInput
-                style={{marginLeft: 10, flexGrow: 1}}
-                placeholder="Email address"
-                keyboardType="email-address"
-              />
+            <View style={[styles.text_container, {marginBottom: 15}]}>
+              <View style={styles.text_box}>
+                <Fontisto
+                  name="mobile-alt"
+                  size={30}
+                  color="#93278f"
+                  style={{alignSelf: 'center'}}
+                />
+                <TextInput
+                  keyboardType="number-pad"
+                  style={{marginLeft: 10, flexGrow: 1}}
+                  placeholder="Mobile number"
+                  maxLength={10}
+                  onChangeText={handleChange('mobile_number')}
+                  onBlur={handleBlur('mobile_number')}
+                  value={values.mobile_number}
+                />
+              </View>
+              {errors.mobile_number ? (
+                <Text style={styles.error_message}>
+                  * {errors.mobile_number}
+                </Text>
+              ) : null}
             </View>
-          </View>
-          <View style={[styles.text_container, {marginBottom: 15}]}>
-            <View style={styles.text_box}>
-              <Fontisto
-                name="key"
-                size={20}
-                color="#93278f"
-                style={{alignSelf: 'center'}}
-              />
-              <TextInput
-                style={{marginLeft: 10, flexGrow: 1}}
-                placeholder="Password"
-              />
+            <View style={[styles.text_container, {marginBottom: 15}]}>
+              <View style={styles.text_box}>
+                <Fontisto
+                  name="email"
+                  size={20}
+                  color="#93278f"
+                  style={{alignSelf: 'center'}}
+                />
+                <TextInput
+                  style={{marginLeft: 10, flexGrow: 1}}
+                  placeholder="Email address"
+                  keyboardType="email-address"
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                />
+              </View>
+              {errors.email ? (
+                <Text style={styles.error_message}>* {errors.email}</Text>
+              ) : null}
             </View>
-          </View>
-          <View style={styles.text_container}>
-            <View style={styles.text_box}>
-              <Fontisto
-                name="key"
-                size={20}
-                color="#93278f"
-                style={{alignSelf: 'center'}}
-              />
-              <TextInput
-                style={{marginLeft: 10, flexGrow: 1}}
-                placeholder="Confirm password"
-              />
+            <View style={[styles.text_container, {marginBottom: 15}]}>
+              <View style={styles.text_box}>
+                <Fontisto
+                  name="key"
+                  size={20}
+                  color="#93278f"
+                  style={{alignSelf: 'center'}}
+                />
+                <TextInput
+                  style={{marginLeft: 10, flexGrow: 1}}
+                  placeholder="Password"
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                />
+              </View>
+              {errors.password ? (
+                <Text style={styles.error_message}>* {errors.password}</Text>
+              ) : null}
             </View>
-          </View>
-          <TouchableOpacity style={styles.button_container}>
-            <Text style={styles.button_text}>Sign up</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.footer}>
-          <Text style={styles.footer_text}>Or</Text>
-          <View style={styles.icon_container}>
-            <TouchableOpacity style={styles.icon_box}>
-              <FontAwesome name="facebook-f" size={30} color="#3b5998" />
+            <View style={styles.text_container}>
+              <View style={styles.text_box}>
+                <Fontisto
+                  name="key"
+                  size={20}
+                  color="#93278f"
+                  style={{alignSelf: 'center'}}
+                />
+                <TextInput
+                  style={{marginLeft: 10, flexGrow: 1}}
+                  placeholder="Confirm password"
+                  onChangeText={handleChange('confirm_password')}
+                  onBlur={handleBlur('confirm_password')}
+                  values={values.confirm_password}
+                />
+              </View>
+              {errors.confirm_password ? (
+                <Text style={styles.error_message}>
+                  * {errors.confirm_password}
+                </Text>
+              ) : null}
+            </View>
+            <TouchableOpacity
+              style={styles.button_container}
+              onPress={handleSubmit}>
+              <Text style={styles.button_text}>Sign up</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.icon_box}>
-              <FontAwesome name="google" size={30} color="#db3236" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.icon_box}>
-              <FontAwesome name="twitter" size={30} color="#00acee" />
-            </TouchableOpacity>
+            <Text style={styles.footer_text}>Or</Text>
+            <View style={styles.icon_container}>
+              <TouchableOpacity style={styles.icon_box}>
+                <FontAwesome name="facebook-f" size={30} color="#3b5998" />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.icon_box}>
+                <FontAwesome name="google" size={30} color="#db3236" />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.icon_box}>
+                <FontAwesome name="twitter" size={30} color="#00acee" />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.account}>
+              Have an account ?{' '}
+              <Text
+                onPress={() => this.props.navigation.replace('Login')}
+                style={styles.login_text}>
+                LOG IN
+              </Text>
+            </Text>
           </View>
-          <Text style={styles.account}>
-            Have an account ? <Text style={styles.login_text}>LOG IN</Text>
-          </Text>
         </View>
-      </View>
-    );
-  }
-}
+      )}
+    </Formik>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -132,7 +186,7 @@ const styles = StyleSheet.create({
     letterSpacing: 2,
   },
   body: {
-    flex: 3,
+    flex: 4,
     paddingHorizontal: 20,
   },
   text_container: {
@@ -190,6 +244,12 @@ const styles = StyleSheet.create({
     color: '#93278f',
     fontWeight: 'bold',
     textDecorationColor: '#93278f',
+  },
+  error_message: {
+    color: 'red',
+    fontSize: 12,
+    fontWeight: 'bold',
+    letterSpacing: 1,
   },
 });
 
